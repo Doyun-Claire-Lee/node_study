e5fef18e936ce278de477bbb369f272c09c5e602
doyun-claire-lee <doyun.lee@kakao.com>
2021-08-24 18:47:55 +0900

    Dwitter

--- dwitter/server/app.js
+++ dwitter/server/app.js
@@ -5,22 +5,31 @@ Dwitter Project
     - 로그인/로그아웃
     - 트윗 생성/수정/삭제
     - 유저별 트윗 조회
-
-2. API 정의
-    - 로그인/로그아웃
-        - /login, /logout
-        - POST
-        - request: user(id, pw) -> session에 저장?
-        - response: null
-    - 트윗 생성/수정/삭제
-        - /create, /update, /delete
-        - GET
-        - request: id, contents, time
-        - response: tweets?
-    - 유저별 트윗 조회
-        - /list
-        - GET
-        - request: id
-        - response: tweets
-
  */
+
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from "helmet";
+
+import tweetsRouter from './router/tweets.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.use('/tweets', tweetsRouter);
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.sendStatus(500);
+});
+app.listen(8080);
+
